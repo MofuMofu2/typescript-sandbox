de0e9aab5e089b420a3003f42a85e446f64f4a73
MofuMofu <froakie002@gmail.com>
2022-07-05 18:53:03 +0900

    feature: 配列からランダムに呼び出した値を格納する

--- src/main.js
+++ src/main.js
@@ -22,6 +22,9 @@ randomize.addEventListener('click', result);
 function result() {
 
   const newStory = storyText;
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
 
   if(customName.value !== '') {
     const name = customName.value;

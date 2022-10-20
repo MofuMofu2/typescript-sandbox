660d72fa2183e799f1ce8d21de85dcb904fe85e2
MofuMofu <froakie002@gmail.com>
2022-10-20 22:10:28 +0900

    fix: #5 ケースを全て通した
    10回目はボーナスがないので、まずはそれを判定する。スペアでもストライクでも3投分足し算する

--- src/bowling.ts
+++ src/bowling.ts
@@ -35,13 +35,19 @@ export const strike = (pins: Frame[]) => {
   let score = 0;
   for (let frame = 0; frame < pins.length; frame++) {
     let knockedPins = frameScore(pins[frame]);
-    const nextFrame = frame + 1
-    const twoFrame = frame + 2
+    const nextFrame = frame + 1;
+    const twoFrame = frame + 2;
+
     if (pins[frame].first === 10) {
-      if(pins[nextFrame].first === 10) {
-        knockedPins += pins[nextFrame].first + pins[twoFrame].first
-      }
-      else {
+      // 10回目かどうか判断する
+      if (frame === 9) {
+        knockedPins = pins[9].first + pins[9].second + pins[9].third!;
+      } else if (pins[nextFrame].first === 10 && frame === 8) {
+        // 9投目
+        knockedPins += pins[9].first + pins[9].second;
+      } else if (pins[nextFrame].first === 10) {
+        knockedPins += pins[nextFrame].first + pins[twoFrame].first;
+      } else {
         knockedPins += frameScore(pins[nextFrame]);
       }
     }

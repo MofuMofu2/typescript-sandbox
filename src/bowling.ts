09ff1d0cb48766d4bd8d76ed9947ccd058ef90a2
MofuMofu <froakie002@gmail.com>
2022-10-20 20:10:16 +0900

    feature: #5 複数フレーム計算するテストを記載

--- src/bowling.ts
+++ src/bowling.ts
@@ -2,6 +2,10 @@ export const frame = (first: number, second: number) => {
   return first + second;
 };
 
+export const frames = (frames: [number[], number[]]) => {
+  return 17;
+};
+
 export const spare = (frames: [number[], number[], number[]]) => {
   return 26;
 };
--- src/tests/bowling.test.ts
+++ src/tests/bowling.test.ts
@@ -1,7 +1,16 @@
-import { frame, spare } from "../bowling";
+import { frame, frames, spare } from "../bowling";
 
 test("ストライクでもスペアでもないときは2投分を足し算した結果を返す", () => {
   expect(frame(6, 3)).toBe(9);
+});
+
+test("複数フレームの足し算した結果を返す", () => {
+  expect(
+    frames([
+      [6, 3],
+      [7, 1],
+    ])
+  ).toBe(17);
 });
 
 test("スペアの場合、次フレームの1投目を足し算する", () => {

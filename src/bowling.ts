6c25d9e89f69ec2c1286463cc93d93437b57f7d4
MofuMofu <froakie002@gmail.com>
2022-11-02 19:56:37 +0900

    refactor: #5 ストライクのときの計算を外に出す
    最終フレーム目は次のフレームという概念がないため、10フレーム目を投げた結果をそのまま足し算しないと多すぎる

--- src/bowling.ts
+++ src/bowling.ts
@@ -4,24 +4,16 @@ type Frame = {
   third?: number;
 };
 
-export const strike = (pins: Frame[]) => {
+export const calcScore = (pins: Frame[]) => {
   let score = 0;
   for (let frame = 0; frame < pins.length; frame++) {
     let knockedPins = frameScore(pins[frame]);
     const nextFrame = frame + 1;
-    const twoFrame = frame + 2;
-
     if (isStrike(pins[frame].first)) {
-      // 10回目かどうか判断する
       if (frame === pins.length - 1) {
-        knockedPins = pins[pins.length - 1].first + pins[pins.length - 1].second + pins[pins.length - 1].third!;
-      } else if (isStrike(pins[nextFrame].first) && frame === (pins.length - 2)) {
-        // 9投目
-        knockedPins += pins[pins.length - 1].first + pins[pins.length - 1].second;
-      } else if (isStrike(pins[nextFrame].first)) {
-        knockedPins += pins[nextFrame].first + pins[twoFrame].first;
+        knockedPins = strike(pins, frame);
       } else {
-        knockedPins += frameScore(pins[nextFrame]);
+        knockedPins += strike(pins, frame);
       }
     } else if (knockedPins === 10) {
       score += pins[nextFrame].first;
@@ -38,4 +30,26 @@ const frameScore = (frame: Frame) => {
 
 const isStrike = (firstShot: number) => {
   return firstShot === 10;
+};
+
+const strike = (pins: Frame[], frame: number) => {
+  let knockedPins = 0;
+  const nextFrame = frame + 1;
+  const twoFrame = frame + 2;
+
+  // 10回目かどうか判断する
+  if (frame === pins.length - 1) {
+    knockedPins =
+      pins[pins.length - 1].first +
+      pins[pins.length - 1].second +
+      pins[pins.length - 1].third!;
+  } else if (isStrike(pins[nextFrame].first) && frame === pins.length - 2) {
+    // 9投目
+    knockedPins += pins[pins.length - 1].first + pins[pins.length - 1].second;
+  } else if (isStrike(pins[nextFrame].first)) {
+    knockedPins += pins[nextFrame].first + pins[twoFrame].first;
+  } else {
+    knockedPins += frameScore(pins[nextFrame]);
+  }
+  return knockedPins;
 };

1e50e5cdcebd435456a5432c41369f6c02d30138
MofuMofu <froakie002@gmail.com>
2022-11-08 21:41:19 +0900

    fix: #15 無配・在庫・通販サイト用の本はないケースがある。これを型で表現した。
    undefinedケアがなくテストが落ちたのでまずは通せるようにした。こういうときはどう書くとスマートなんだろう。

--- src/calcBookSales.ts
+++ src/calcBookSales.ts
@@ -1,17 +1,29 @@
 type FanBook = {
   total: number;
-  sample: number;
-  stock: number;
-  market: number;
+  sample?: number;
+  stock?: number;
+  market?: number;
   price: number;
 };
 
 type FanBooks = FanBook[];
 
 export const countBookSales = (bookSale: FanBook) => {
-  return bookSale.total - (bookSale.sample + bookSale.stock + bookSale.market);
+  return bookSale.total - forSaleBooks(bookSale);
 };
 
 export const calcBookSales = (bookSale: FanBook) => {
   return countBookSales(bookSale) * bookSale.price;
+};
+
+const forSaleBooks = (book: FanBook) => {
+  if (book.sample && book.stock && book.market) {
+    return book.sample + book.stock + book.market;
+  } else if (book.sample && book.stock) {
+    return book.sample + book.stock;
+  } else if (book.sample) {
+    return book.sample;
+  } else {
+    return 0;
+  }
 };
--- src/tests/calcBookSales.test.ts
+++ src/tests/calcBookSales.test.ts
@@ -1,25 +1,32 @@
 import { countBookSales, calcBookSales } from "../calcBookSales";
 
-test("売り上げ数 = 持ち込み数 - (無配 + 残り + 通販サイト送付分）", () => {
-  expect(
-    countBookSales({
-      total: 306,
-      sample: 3,
-      stock: 80,
-      market: 164,
-      price: 1000,
-    })
-  ).toBe(59);
-});
+const fanbooks = [
+  {
+    total: 306,
+    sample: 3,
+    stock: 80,
+    market: 164,
+    price: 1000,
+  },
+  {
+    total: 104,
+    sample: 2,
+    stock: 32,
+    market: 35,
+    price: 1000,
+  },
+  {
+    total: 20,
+    price: 500,
+  },
+];
 
-test("売り上げ金額 = 売り上げ数 * 売価", () => {
-  expect(
-    calcBookSales({
-      total: 306,
-      sample: 3,
-      stock: 80,
-      market: 164,
-      price: 1000,
-    })
-  ).toBe(59000);
+describe("一冊の同人誌の売り上げ数と売上額を計算する", () => {
+  test("売り上げ数 = 持ち込み数 - (無配 + 残り + 通販サイト送付分）", () => {
+    expect(countBookSales(fanbooks[0])).toBe(59);
+  });
+
+  test("売り上げ金額 = 売り上げ数 * 売価", () => {
+    expect(calcBookSales(fanbooks[0])).toBe(59000);
+  });
 });

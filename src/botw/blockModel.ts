45820f09c71920e855300295426475a716aa7749
MofuMofu <froakie002@gmail.com>
2022-12-12 21:37:48 +0900

    feature: #22 ダメージが1ポイント以上と0ポイント以下だった場合で結果が変わるためケースを分ける

--- src/botw/blockModel.ts
+++ src/botw/blockModel.ts
@@ -1,3 +1,12 @@
 export const enemyAttack = (enemy: number, weapon: number) => {
   return enemy + weapon;
 };
+
+export const calcBlock = (attack: number, armor: number) => {
+  return calcDamage(attack, armor) * 0.25;
+};
+
+const calcDamage = (attack: number, armor: number) => {
+  const point = attack - armor;
+  return point > 0 ? point : 1;
+};

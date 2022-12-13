f3c3de425892a5890c06e006561b28925db0cf31
MofuMofu <froakie002@gmail.com>
2022-12-13 20:22:29 +0900

    feature: #22 0.25計算だと、ハート1/4分の表記をするとき追加で変換が必要なことに気づいた。
    最終的な形はポイントで換算する方が扱いやすいのではと思いダメージ数値がそのまま返却されるように修正

--- src/botw/blockModel.ts
+++ src/botw/blockModel.ts
@@ -3,10 +3,10 @@ export const enemyAttack = (enemy: number, weapon: number) => {
 };
 
 export const calcBlock = (attack: number, armor: number) => {
-  return calcDamage(attack, armor) * 0.25;
+  const point = calcDamage(attack, armor);
+  return point > 0 ? point : 1;
 };
 
 const calcDamage = (attack: number, armor: number) => {
-  const point = attack - armor;
-  return point > 0 ? point : 1;
+  return attack - armor;
 };

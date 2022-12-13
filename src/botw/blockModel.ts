f4f8a670b889a2482f00db44d2b553711ab07645
MofuMofu <froakie002@gmail.com>
2022-12-13 20:49:57 +0900

    refactor: #24 追加したロジックを活用して最終的なダメージ計算を可能とするため、I/Fを変更した

--- src/botw/blockModel.ts
+++ src/botw/blockModel.ts
@@ -1,3 +1,10 @@
+type Battle = {
+  enemy: number;
+  weapon: number;
+  armor: number;
+  meal: number;
+};
+
 export const enemyAttack = (enemy: number, weapon: number) => {
   return enemy + weapon;
 };
@@ -6,11 +13,14 @@ export const blockPower = (armor: number, meal: number) => {
   return armor + meal;
 };
 
-export const calcBlock = (attack: number, armor: number) => {
-  const point = calcDamage(attack, armor);
+export const calcBlock = (battle: Battle) => {
+  const point = calcDamage(
+    enemyAttack(battle.enemy, battle.weapon),
+    blockPower(battle.armor, battle.meal)
+  );
   return point > 0 ? point : 1;
 };
 
-const calcDamage = (attack: number, armor: number) => {
-  return attack - armor;
+const calcDamage = (attack: number, block: number) => {
+  return attack - block;
 };

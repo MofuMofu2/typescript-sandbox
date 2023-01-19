b1614c0c5520811327cba96e098eee927f28e75b
MofuMofu <froakie002@gmail.com>
2023-01-19 19:48:05 +0900

    feature: #28 enumで代替する

--- src/practice-types/enum.ts
+++ src/practice-types/enum.ts
@@ -8,3 +8,14 @@ enum KantoPokemon {
 const firstPokemon = KantoPokemon.Bulbasaur;
 
 console.log(firstPokemon); // 1
+
+// as constを使って書き換える
+
+// 配列を宣言する
+const johtoPokemon = ["Chikorita", "Bayleef", "Meganium"];
+
+// as constキーワードで 反復処理可能なunion型を生成する
+type JohtoPokemon = typeof johtoPokemon[number];
+
+// 配列に直接アクセスする
+console.log(johtoPokemon[0]); // Chikorita

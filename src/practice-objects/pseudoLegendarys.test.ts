80033fd092c8c4b12f24dd29f4ee2671892eca27
MofuMofu <froakie002@gmail.com>
2023-07-19 08:44:05 +0900

    feature: #40 第3世代までの600族を返す配列を作成

--- src/practice-objects/pseudoLegendarys.test.ts
+++ src/practice-objects/pseudoLegendarys.test.ts
@@ -1,10 +1,19 @@
-import { createPseudoLegendaryInfo } from "./pseudoLegendarys";
+import * as operator from "./pseudoLegendarys";
 import dragonite from "../assets/149.json";
 
 it("600族のポケモンの情報をPokeAPIから取り出せる", () => {
-  expect(createPseudoLegendaryInfo(dragonite)).toEqual({
+  expect(operator.createPseudoLegendaryInfo(dragonite)).toEqual({
     id: 149,
     name: "dragonite",
     types: ["dragon", "flying"],
   });
+});
+
+it("第三世代までの600族のポケモンの配列を返す", () => {
+  expect(operator.through3()).toEqual([
+    { id: 149, name: "dragonite", types: ["dragon", "flying"] },
+    { id: 248, name: "tyranitar", types: ["rock", "dark"] },
+    { id: 373, name: "salamence", types: ["dragon", "flying"] },
+    { id: 376, name: "metagross", types: ["steel", "psychic"] },
+  ]);
 });

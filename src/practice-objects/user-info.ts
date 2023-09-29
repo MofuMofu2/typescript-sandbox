b8db7d41f07541c83164613e9bb1ec5ebdef1a96
MofuMofu <froakie002@gmail.com>
2023-09-29 22:02:58 +0900

    delete: #66 usersは未使用なので削除。これESLintで検出しないのかになった。バージョン違いのせい？

--- src/practice-objects/user-info.ts
+++ src/practice-objects/user-info.ts
@@ -4,12 +4,6 @@ type UserInfo = {
   isAdmin: boolean;
 };
 
-const users: UserInfo[] = [
-  { id: 1, name: "John", isAdmin: true },
-  { id: 2, name: "Jane", isAdmin: false },
-  { id: 3, name: "Doe", isAdmin: true },
-];
-
 export const filterAdminUsers = (userLists: UserInfo[]): UserInfo[] => {
   return userLists.filter((user) => {
     return user.isAdmin === true;

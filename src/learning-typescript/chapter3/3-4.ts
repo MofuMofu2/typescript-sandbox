f9d62b05a57626351414810c6c59913891bfb881
MofuMofu2 <froakie002@gmail.com>
2024-11-01 20:10:48 +0900

    3-4-1 真偽性による絞り込みのテストコードを追加
    実際は?で推論してAPIつける気がするけど、絞り込みの説明なのでしょうがない気がする

--- src/learning-typescript/chapter3/3-4.ts
+++ src/learning-typescript/chapter3/3-4.ts
@@ -2,3 +2,13 @@ export const strictNullCheck = (num: number) => {
   let nameMaybe = num > 0.5 ? "TONY" : undefined;
   return nameMaybe?.toLowerCase();
 };
+
+export const truthinessCheck = (num: number) => {
+  let geneticist = num > 0.5 ? "Barbara" : undefined;
+
+  if (geneticist) {
+    geneticist = geneticist.toUpperCase();
+  }
+
+  return geneticist;
+};
